c2866c1a4befb49cd53983fa066363cdaa8a41df
Rushabh Sanghvi <rushabhsanghvi@SV-LT-1451.local>
2018-07-18 14:36:32 -0700

    Fixing spire and sidecar startup

--- beatrice/install_sidecar.sh
+++ beatrice/install_sidecar.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-cd $HOME
+cd /extra_mount/misc_config/
 curl --silent --location https://github.com/spiffe/sidecar/releases/download/0.1/sidecar_0.1_linux_amd64.tar.gz | tar xzf -
-
+./sidecar
