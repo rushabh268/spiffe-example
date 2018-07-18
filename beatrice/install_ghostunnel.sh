49f98e29d9c3ba0f45fe64703dd137dec048da5a
Rushabh Sanghvi <rushabhsanghvi@SV-LT-1451.local>
2018-07-18 13:54:42 -0700

    Fixing cherry pick

--- beatrice/install_ghostunnel.sh
+++ beatrice/install_ghostunnel.sh
@@ -30,4 +30,3 @@ sudo cp -r /home/vagrant/go/src/github.com/spiffe/ghostunnel /extra_mount/blog/c
 
 # abusing .bash_aliases to ammend PATH, for convenience
 #sudo echo "export PATH=/usr/local/go/bin:/home/ubuntu/go/bin:$PATH" > /home/ubuntu/.bash_aliases
-
