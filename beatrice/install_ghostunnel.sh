bc872a23f6e146c476ae571661ddf599d7696160
Rushabh Sanghvi <rushabhsanghvi@SV-LT-1451.local>
2018-07-18 10:35:47 -0700

    Fixing GO dependencies in ghostunnel

--- beatrice/install_ghostunnel.sh
+++ beatrice/install_ghostunnel.sh
@@ -9,22 +9,25 @@ GHOSTUNNEL_BRANCH=spiffe-support
 
 sudo apt-get -y install build-essential libltdl-dev git
 
-export PATH=/usr/local/go/bin:/home/ubuntu/go/bin:$PATH
-
+#export PATH=/usr/local/go/bin:/home/vagrant/go/bin:$PATH
 # ghostunnel requires golang1.9, so we fetch a tarball
 
 curl --silent $GOLANG_URL | sudo tar --directory /usr/local -xzf -
 
-
-sudo rm -rf /home/ubuntu/go
-sudo mkdir -p /home/ubuntu/go/src/github.com/spiffe
-cd /home/ubuntu/go/src/github.com/spiffe
+export GOROOT=/usr/local/go
+export GOPATH=$HOME/go
+export PATH=$GOPATH/bin:$GOROOT/bin:$PATH
+sudo rm -rf /home/vagrant/go
+sudo mkdir -p /home/vagrant/go/src/github.com/spiffe
+cd /home/vagrant/go/src/github.com/spiffe
+go get ./.
+#cd src/github.com/spiffe
 sudo git clone --branch $GHOSTUNNEL_BRANCH  https://github.com/spiffe/ghostunnel.git
 cd ghostunnel
 go install
 
 # send a copy to our container friend
-sudo cp -r /home/ubuntu/go/src/github.com/spiffe/ghostunnel /extra_mount/blog/container_ghostunnel/
+sudo cp -r /home/vagrant/go/src/github.com/spiffe/ghostunnel /extra_mount/blog/container_ghostunnel/
 
 # abusing .bash_aliases to ammend PATH, for convenience
 #sudo echo "export PATH=/usr/local/go/bin:/home/ubuntu/go/bin:$PATH" > /home/ubuntu/.bash_aliases
